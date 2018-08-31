849649a0d5e53950f2d6155156882a7e4cdba8ab
Daniel Foster <danieljfoster87@gmail.com>
2018-08-31 19:18:05 -0400

    restyled contact page

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -12,82 +12,104 @@ class ContactCard extends Component {
   render() {
     return (
       <div>
-        <div className="container-fluid p-5">
-          <div className="row align-content-center">
-          <div className="col-md-3">
-              <a href="mailto:danieljfoster87@gmail.com">
-                <img
-                  src={Gmail}
-                  className="img-fluid contact-image p-3"
-                  alt="gmail"
-                />
-              </a>
+        <div className="container text-center mt-5">
+          <div className="row justify-content-center">
+            <div className="col-md-12">
+              <div className="mb-5">
+                <div className="">
+                  <h1 className="contact-header">Professional</h1>
+                </div>
+                <div className="mt-5">
+                  <div className="row">
+                    <div className="col-md-3 col-6">
+                      <a href="mailto:danieljfoster87@gmail.com">
+                        <img
+                          src={Gmail}
+                          className="img-fluid contact-image p-3"
+                          alt="gmail"
+                        />
+                      </a>
+                    </div>
+
+                    <div className="col-md-3 col-6">
+                      <a href="https://www.linkedin.com/in/dan-foster-8a2503126/">
+                        <img
+                          src={Linkedin}
+                          className="img-fluid contact-image p-3"
+                          alt="linkedin"
+                        />
+                      </a>
+                    </div>
+                    <div className="col-md-3 col-6">
+                      <a href="https://github.com/dfoster29">
+                        <img
+                          src={Github}
+                          className="img-fluid contact-image p-3"
+                          alt="github"
+                        />
+                      </a>
+                    </div>
+                    <div className="col-md-3 col-6">
+                      <a href="https://stackoverflow.com/users/9247974/daniel-foster?tab=profile">
+                        <img
+                          src={StackOverflow}
+                          className="img-fluid contact-image p-3"
+                          alt="stack overflow"
+                        />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
 
-            <div className="col-md-3">
-              <a href="https://www.linkedin.com/in/dan-foster-8a2503126/">
-                <img
-                  src={Linkedin}
-                  className="img-fluid contact-image p-3"
-                  alt="linkedin"
-                />
-              </a>
+            <div className="col-md-12">
+              <div className="mb-5">
+                <div className="">
+                  <h1 className="contact-header">Personal</h1>
+                </div>
+                <div className="mt-5">
+                  <div className="row">
+                    <div className="col-md-3 col-6">
+                      <a href="https://www.instagram.com/fosterthedan/">
+                        <img
+                          src={Instagram}
+                          className="img-fluid contact-image p-3"
+                          alt="instagram"
+                        />
+                      </a>
+                    </div>
+                    <div className="col-md-3 col-6">
+                      <a href="https://www.facebook.com/danfoster29">
+                        <img
+                          src={Facebook}
+                          className="img-fluid contact-image p-3"
+                          alt="facebook"
+                        />
+                      </a>
+                    </div>
+                    <div className="col-md-3 col-6">
+                      <a href="https://open.spotify.com/user/12146437690?si=16ZNT_WZQ4yHmbmoT7LvWQ&nd=1">
+                        <img
+                          src={Spotify}
+                          className="img-fluid contact-image p-3"
+                          alt="spotify"
+                        />
+                      </a>
+                    </div>
+                    <div className="col-md-3 col-6">
+                      <a href="https://twitter.com/dfost29">
+                        <img
+                          src={Twitter}
+                          className="img-fluid contact-image p-3"
+                          alt="twitter"
+                        />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="col-md-3">
-              <a href="https://github.com/dfoster29">
-                <img
-                  src={Github}
-                  className="img-fluid contact-image p-3"
-                  alt="github"
-                />
-              </a>
-            </div>
-            <div className="col-md-3">
-              <a href="https://stackoverflow.com/users/9247974/daniel-foster?tab=profile">
-                <img
-                  src={StackOverflow}
-                  className="img-fluid contact-image p-3"
-                  alt="stack overflow"
-                />
-              </a>
-            </div>
-            <div className="col-md-3">
-              <a href="https://www.instagram.com/fosterthedan/">
-                <img
-                  src={Instagram}
-                  className="img-fluid contact-image p-3"
-                  alt="instagram"
-                />
-              </a>
-            </div>
-            <div className="col-md-3">
-              <a href="https://www.facebook.com/danfoster29">
-                <img
-                  src={Facebook}
-                  className="img-fluid contact-image p-3"
-                  alt="facebook"
-                />
-              </a>
-            </div>
-            <div className="col-md-3">
-              <a href="https://open.spotify.com/user/12146437690?si=16ZNT_WZQ4yHmbmoT7LvWQ&nd=1">
-                <img
-                  src={Spotify}
-                  className="img-fluid contact-image p-3"
-                  alt="spotify"
-                />
-              </a>
-            </div>
-            <div className="col-md-3">
-              <a href="https://twitter.com/dfost29">
-                <img
-                  src={Twitter}
-                  className="img-fluid contact-image p-3"
-                  alt="twitter"
-                />
-              </a>
-            </div>
-
           </div>
         </div>
       </div>
